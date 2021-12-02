0dcffb374e7b0199578c0ffb8f22e24083e7f7a1
ngoctrin <95402086+ngoctrin@users.noreply.github.com>
2021-12-02 13:34:54 +0700

    Add files via upload

---
+++ Untitled-1.ps1
@@ -0,0 +1,4 @@
+Write-Host "Chào mừng bạn tới với Quantrimang! Script đầu tiên của bạn đã khởi chạy thành công!"
+Chào mừng bạn tới với Quantrimang! Script đầu tiên của bạn đã khởi chạy thành công!
+run
+Write-Host "Congratulations! Your first script executed successfully"
